1cd115a51406d7520c1c804d57eeb1007c03c0c9
Panith <sopanith.meach@restartasia.com>
2014-10-21 17:58:59 +0700

    Mocking up

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -11,5 +11,21 @@
 		{{HTML::image('images/feature.png','Job Feature',array('class'=>'img-responsive'))}}
 	</div>
 </section>
-	
+<section class="latest-job">
+	<div class="masthead-item background-cover" style="background-image:url('{{asset('images/latest-job.png')}}');">
+			
+	</div>
+</section>
+<section class="mobile-friendly">
+	{{HTML::image('images/mobile-friendly.png','Mobile Friendly',array('class'=>'img-responsive'))}}
+</section>
+<section class="partner">
+	{{HTML::image('images/partner.png','Job Footer',array('class'=>'img-responsive'))}}
+</section>
+<section class="footer">
+	{{HTML::image('images/footer.png','Job Footer',array('class'=>'img-responsive'))}}
+</section>
+
+
+
 @stop

4345c814cf95d9d976267f020939c47f26c86505
Panith <sopanith.meach@restartasia.com>
2014-10-27 10:20:21 +0700

    Candidate with popup

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -13,12 +13,24 @@
 </section>
 <section class="latest-candidate">
 	<div class="masthead-item background-cover" style="background-image:url('{{asset('images/testimonals-bg-darker.jpg')}}');">
+		<div class="col-md-8 container col-md-offset-2">
 			
+			@for($i=1; $i<=40; $i++)
+			<a href="#" class="ui-state-default radius-all-2 display-block  popover-button-default" data-content="<div class='pad10A'>
+				Content For popup
+			</div>" title="" data-trigger="hover" data-placement="top" data-original-title="Candidate's Name">
+				<img src="http://placehold.it/70">
+			</a>
+			@endfor
+			<a href="#" class="btn large transparent" title="">
+            <span class="button-content">Brows More</span>
+        </a>
+		</div>
 	</div>
 </section>
 <section class="latest-job">
 	<div class="masthead-item background-cover" style="background-image:url('{{asset('images/latest-job.png')}}');">
-			
+		
 	</div>
 </section>
 <section class="mobile-friendly">
